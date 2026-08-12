60596e171b78c2cb1bf94a412d82c1721768207e
Samueru-sama <xdglawyer@outlook.com>
2026-08-11 20:31:26 -0400

    package app

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,16 +3,28 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q opentoonz | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/icons/hicolor/256x256/apps/io.github.OpenToonz.png
+export DESKTOP=/usr/share/applications/io.github.OpenToonz.desktop
+
+# change the binary in the .desktop to launch OpenToonz directly
+# setup-opentoonz.hook does the same as the opentoonz shell script
+sed -i -e 's|Exec=opentoonz|Exec=OpenToonz|g' "$DESKTOP"
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+LD_LIBRARY_PATH=/usr/lib/opentoonz quick-sharun \
+	/usr/bin/OpenToonz       \
+	/usr/bin/tcleanup        \
+	/usr/bin/tcomposer       \
+	/usr/bin/tconverter      \
+	/usr/bin/tfarmcontroller \
+	/usr/bin/tfarmserver     \
+	/usr/lib/opentoonz       \
+	/usr/share/opentoonz
 
 # Additional changes can be done in between here
 
